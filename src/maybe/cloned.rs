4a30396b6d4105cda4807018011c782afe17ebbd
mandober <ilicivan@zoho.com>
2018-02-24 15:52:33 +0100

    cloned

--- src/maybe/cloned.rs
+++ src/maybe/cloned.rs
@@ -1,22 +1,24 @@
+//import
 use maybe::Maybe::{self, Nothing, Just};
 
+//clone 1
 impl<'a, T> Maybe<&'a T> where T: Clone {
   /// Maps Maybe<&T> to Maybe<T> by cloning the contents
   pub fn cloned(self) -> Maybe<T> {
     match self {
       Just(v) => Just(v.clone()),
-      Nothing => Nothing,
+      Nothing => Nothing
     }
   }
 }
 
-
+//clone 2
 impl<'a, T> Maybe<&'a mut T> where T: Clone {
   /// Maps Maybe<&mut T> to Maybe<T> by cloning the contents
   pub fn cloned(self) -> Maybe<T> {
     match self {
       Just(v) => Just(v.clone()),
-      Nothing => Nothing,
+      Nothing => Nothing
     }
   }
 }
